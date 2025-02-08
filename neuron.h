11472005cac24c2fe6b45f9a748dce17e8685fca
Hopelyze <164333303+Hopelyze@users.noreply.github.com>
2025-02-08 12:03:07 +0100

    Update neuron.h

--- neuron.h
+++ neuron.h
@@ -1,1 +1,13 @@
+// neuron.h
+#ifndef NEURON_H
+#define NEURON_H
+
+#define INPUT_SIZE 2 // Nombre d'entrées (x et y)
+
+typedef struct {
+    double weights[INPUT_SIZE]; // tableau contenant les poids synaptiques
+} Neuron;
+
+double transfer(double x); // Fonction de transfert
+double forward(Neuron *neuron, double inputs[INPUT_SIZE]); // calcul de la sortie
 
