5aee813c07ad8ad2ac74872d06adde0a67ae17ae
Hopelyze <164333303+Hopelyze@users.noreply.github.com>
2025-02-08 12:02:25 +0100

    add transfert & forward functions neuron

--- neuron.c
+++ neuron.c
@@ -1,1 +1,19 @@
+#include "neuron.h"
+#include <stdlib.h>
+#include <math.h>
+
+// Fonction d'activation : Tangente hyperbolique
+double transfer(double x) {
+    return tanh(x);
+}
+
+// Fonction de propagation avant
+double forward(Neuron *neuron, double inputs[INPUT_SIZE]) {
+    double sum = 0;
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        //multiplication des poids par les entrées correspondantes
+        sum += neuron->weights[i] * inputs[i];
+    }
+    return transfer(sum);
+}
 
