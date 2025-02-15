5a4055ba855b48dd5772311a194e0707c2e9f0f1
Hopelyze <164333303+Hopelyze@users.noreply.github.com>
2025-02-15 19:09:08 +0100

    add fonctions couche & reseau

--- neuron.c
+++ neuron.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Fonction d'activation : Tangente hyperbolique
+// Fonction de transfert : Tangente hyperbolique
 double transfer(double x) {
     return tanh(x);
 }
 
-// Fonction de propagation avant
+// Fonction de propagation 
 double forward(Neuron *neuron, double inputs[INPUT_SIZE]) {
     double sum = 0;
     for (int i = 0; i < INPUT_SIZE; i++) {
@@ -16,4 +16,82 @@ double forward(Neuron *neuron, double inputs[INPUT_SIZE]) {
     }
     return transfer(sum);
 }
+
+
+Couche *init_couche(int nb_neurones, Couche *couche_suivante, int is_fst_couche, int is_lst_couche) {
+    Couche *couche = malloc(sizeof(Couche));
+    if (!couche) {
+        printf("Erreur d'allocation mémoire\n");
+        exit(1);
+    }
+	// Initialisation du tableau de neurones
+    couche->nb_neurones = nb_neurones;
+    couche->tab_n = malloc(nb_neurones * sizeof(Neuron));
+    if (!couche->tab_n) {
+        printf("Erreur d'allocation mémoire\n");
+        exit(1);
+    }
+	// Initialisation des poids pour chaque neurone
+    for (int i = 0; i < nb_neurones; i++) {
+        couche->tab_n[i].weights = malloc(INPUT_SIZE * sizeof(double));
+        for (int j = 0; j < INPUT_SIZE; j++) {
+            couche->tab_n[i].weights[j] = ((double)rand() / RAND_MAX) * 2 - 1; 
+        }
+    }
+    couche->p = couche_suivante;
+    couche->is_fst_couche = is_fst;
+    couche->is_lst_couche = is_lst;
+    return couche;
+}
+
+
+double *calcul_couche(Couche *couche, double *tab_val, int input_size) {
+	//allocation du tab qui stocke la sortie de chaque neurone de la couche
+    double *tab_result = malloc(couche->nb_neurones * sizeof(double));
+    if (!tab_result) {
+        printf("Erreur d'allocation mémoire\n");
+        exit(1);
+    }
+		
+	//pour chaque neurone on applique la fonction de propagation
+    for (int i = 0; i < couche->nb_neurones; i++) {
+        tab_result[i] = forward(&couche->tab_n[i], tab_val);
+    }
+    return tab_result;
+}
+
+
+double *calcul_reseau(double *tab_val, Couche *fst_couche) {
+    Couche *current = fst_couche;
+    double *result = tab_val; 
+    while (current) { // tant qu'on n'est pas à la dernière couche => sorties deviennent les entrées de couche suivante.
+        result = calcul_couche(current, result, INPUT_SIZE); //stocke tempo les sorties de chaque couche avant d’être transmises à suivante
+        current = current->p;
+    }
+    return result;
+}
+
+
+Couche *init_reseau(int nb_couches, int taille_max, int taille_min, int nb_entrees, 
+int nb_sorties) {
+	// creation couche de sortie (nb_sorties = nb_neurones)
+    Couche *lst_couche = init_couche(nb_sorties, NULL, 0, 1);
+    Couche *temp = lst_couche;
+	// ajout des couches cachées (leurs nb de neurones est aléatoire)
+    for (int i = 1; i < nb_couches - 1; i++) {
+        int taille_courante = rand() % (taille_max - taille_min + 1) + taille_min;
+        //chaque ouvelle couche pointe vers la précédente
+        temp = init_couche(taille_courante, temp, 0, 0);
+    }
+    Couche *fst_couche = init_couche(nb_entrees, temp, 1, 0); 
+    return fst_couche;
+}
+
+void interpretation(double *tab_val_sortie, char *tab_sortie[], int len) {
+    for (int i = 0; i < len; i++) {
+        printf("Le point a une probabilité de %f d’être %s\n", tab_val_sortie[i], tab_sortie[i]);
+    }
+}
+
+
 
