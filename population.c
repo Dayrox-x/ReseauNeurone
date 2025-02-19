611ab5e1a65d7d6a1ebd1f55770f861f8314731f
LordWe4pOns <simon.cossais@orange.fr>
2025-02-19 10:38:28 +0100

    affichage spirale bleu et rouge

--- population.c
+++ population.c
@@ -72,15 +72,7 @@ Pixel getPixel(Image image, int x, int y) {
     return image->tab[x][y];
 }
 
-<<<<<<< HEAD
 void setPixel(Image image, int x, int y, Pixel p) {
-=======
-void setPixel(Image *image, int x, int y, Pixel p) {
-    if (x < 0 || x >= image->width || y < 0 || y >= image->height) {
-        printf("Erreur: setPixel hors limites (%d, %d) !\n", x, y);
-        return;
-    }
->>>>>>> 32885d9071a9236770d672b5f235472b6b2ac7f7
     image->tab[x][y] = p;
 }
 
@@ -106,14 +98,9 @@ void setPixelColor(Image image, int x, int y, Color c) {
 Pixel ** createPixelTab(int width, int height, Color c) {
     Pixel ** tab = (Pixel**)malloc(sizeof(Pixel*) * width);
     for (int i = 0; i < width; i++) {
-<<<<<<< HEAD
         tab[i] = (Pixel*)malloc(sizeof(Pixel) * height);
         for (int j = 0; j < height; j++) {
             tab[i][j] = createPixel(i, j, c);
-=======
-        for (int j = 0; j < height; j++) {
-            tab[i][j] = createPixel(i, j, createColor(0, 0, 0)); // Pixels noirs par défaut
->>>>>>> 32885d9071a9236770d672b5f235472b6b2ac7f7
         }
     }
     return tab;
@@ -128,26 +115,17 @@ Image createImage(int width, int height, Color c) {
 }
 
 void destroyImage(Image image) {
-<<<<<<< HEAD
     for (int i = 0; i < image->width; i++) {
         for (int j = 0; j < image->height; j++) {
             destroyPixel(image->tab[i][j]);
         }
         free(image->tab[i]);
-=======
-    if (image.tab == NULL) return;
-    for (int i = 0; i < image.width; i++) {
-        if (image.tab[i] != NULL) {
-            free(image.tab[i]);
-        }
->>>>>>> 32885d9071a9236770d672b5f235472b6b2ac7f7
     }
     free(image->tab);
     free(image);
 }
 
 void createPopulation(Image image, int populationSize, Color c, bool val) {
-<<<<<<< HEAD
     int sign;
     if (val){sign = 1;} else {sign = -1;}
     int x = 0, y = 0;
@@ -161,21 +139,5 @@ void createPopulation(Image image, int populationSize, Color c, bool val) {
             setPixelColor(image, x, y, c);
         }
         i += 0.2;
-=======
-    int width = getWidth(image);
-    int height = getHeight(image);
-
-    for (int i = 0; i < populationSize; i++) {
-        int x = width / 2 + (int)(50 * cos(i)); 
-        int y = height / 2 + (int)(50 * sin(i));
-
-        if (x < 0 || x >= width || y < 0 || y >= height) {
-            printf("Coordonnées invalides : x=%d, y=%d, ignoré.\n", x, y);
-            continue;
-        }
-
-        Pixel p = createPixel(x, y, c);
-        setPixel(&image, x, y, p);
->>>>>>> 32885d9071a9236770d672b5f235472b6b2ac7f7
     }
 }
