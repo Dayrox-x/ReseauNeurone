017978535ff757efae884fbdf5861b61d65ff9d7
LordWe4pOns <simon.cossais@orange.fr>
2025-02-19 10:27:47 +0100

    affichage spirale bleu et rouge

--- population.c
+++ population.c
@@ -14,11 +14,15 @@ int getG(Color c){return c->g;}
 void setB(Color c, int b){c->b = b;}
 int getB(Color c){return c->b;}
 
-Color createColor(int r, int g, int b) {
-    Color c = (Color)malloc(sizeof(col));
+void setA(Color c, int a){c->a = a;}
+int getA(Color c){return c->a;}
+
+Color createColor(int r, int g, int b, int a) {
+    Color c = (Color)malloc(sizeof(SDL_Color));
     setR(c, r);
     setG(c, g);
     setB(c, b);
+    setA(c, a);
     return c;
 }
 
@@ -27,82 +31,112 @@ void destroyColor(Color c) {free(c);}
 
 // getters setters de Pixel
 void setColor(Pixel p, Color c) {
-    p.color = c;
+    p->color = c;
 }
 
 Color getColor(Pixel p) {
-    return p.color;
+    return p->color;
 }
 
 void setX(Pixel p, int x) {
-    p.x = x;
+    p->x = x;
 }
 
 int getX(Pixel p) {
-    return p.x;
+    return p->x;
 }
 
 void setY(Pixel p, int y) {
-    p.y = y;
+    p->y = y;
 }
 
 int getY(Pixel p) {
-    return p.y;
+    return p->y;
 }
 
 Pixel createPixel(int x, int y, Color c) {
-    Pixel p;
+    Pixel p = malloc(sizeof(pxl));
     setX(p, x);
     setY(p, y);
     setColor(p, c);
     return p;
 }
 
+void destroyPixel(Pixel p) {
+    free(p);
+}
+
 // getters setters de Image
 
 Pixel getPixel(Image image, int x, int y) {
-    return image.tab[x][y];
+    return image->tab[x][y];
 }
 
 void setPixel(Image image, int x, int y, Pixel p) {
-    image.tab[x][y] = p;
+    image->tab[x][y] = p;
 }
 
 int getWidth(Image image) {
-    return image.width;
+    return image->width;
 }
 
 int getHeight(Image image) {
-    return image.height;
+    return image->height;
+}
+
+Color getPixelColor(Image image, int x, int y) {
+    return getColor(getPixel(image, x, y));
+}
+
+void setPixelColor(Image image, int x, int y, Color c) {
+    setColor(getPixel(image, x, y), c);
 }
 
 
-Pixel ** createPixelTab(int width, int height) {
+
+Pixel ** createPixelTab(int width, int height, Color c) {
     Pixel ** tab = (Pixel**)malloc(sizeof(Pixel*) * width);
     for (int i = 0; i < width; i++) {
         tab[i] = (Pixel*)malloc(sizeof(Pixel) * height);
+        for (int j = 0; j < height; j++) {
+            tab[i][j] = createPixel(i, j, c);
+        }
     }
     return tab;
 }
 
-Image createImage(int width, int height) {
-    Image image;
-    image.width = width;
-    image.height = height;
-    image.tab = createPixelTab(width, height);
+Image createImage(int width, int height, Color c) {
+    Image image = (Image)malloc(sizeof(img));
+    image->width = width;
+    image->height = height;
+    image->tab = createPixelTab(width, height, c);
     return image;
 }
 
 void destroyImage(Image image) {
-    for (int i = 0; i < image.width; i++) {
-        free(image.tab[i]);
+    for (int i = 0; i < image->width; i++) {
+        for (int j = 0; j < image->height; j++) {
+            destroyPixel(image->tab[i][j]);
+        }
+        free(image->tab[i]);
     }
-    free(image.tab);
+    free(image->tab);
+    free(image);
 }
 
 void createPopulation(Image image, int populationSize, Color c, bool val) {
-    for (int i = 0; i < populationSize; i++){
-        Pixel p = createPixel(image.width / 2 + i * (int)cos((double)i), image.height / 2 + i * (int)sin((double)i), c);
-        setPixel(image, getX(p), getY(p), p);
+    int sign;
+    if (val){sign = 1;} else {sign = -1;}
+    int x = 0, y = 0;
+    float i = 0;
+    while (x < image->width && y < image->height && x >= 0 && y >= 0) {
+        float cos_x = cos(i);
+        float sin_y = sin(i);
+        x = (image->width / 2 + sign * (int)(i * cos_x));
+        y = (image->height / 2 + sign * (int)(i * sin_y));
+        if (x < image->width && y < image->height && x >= 0 && y >= 0) {
+            setPixelColor(image, x, y, c);
+        }
+        i += 0.2;
     }
 }
--- population.h
+++ population.h
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
 
-typedef struct {
-    int r, g, b;
-} col;
-
-typedef col* Color;
+typedef SDL_Color* Color;
 
 typedef struct {
     int x;
     int y;
     Color color; // RGB
-} Pixel;
+} pxl;
+
+typedef pxl* Pixel;
 
 typedef struct {
     Pixel** tab;
     int width, height;
-} Image;
+} img;
+
+typedef img* Image;
 
 // getters setters de Color
 void setR(Color c, int r);
@@ -29,7 +30,10 @@ int getG(Color c);
 void setB(Color c, int b);
 int getB(Color c);
 
-Color createColor(int r, int g, int b);
+void setA(Color c, int a);
+int getA(Color c);
+
+Color createColor(int r, int g, int b, int a);
 void destroyColor(Color c);
 
 // getters setters de Pixel
@@ -43,6 +47,7 @@ void setY(Pixel p, int y);
 int getY(Pixel p);
 
 Pixel createPixel(int x, int y, Color c);
+void destroyPixel(Pixel p);
 
 // getters setters de Image
 Pixel getPixel(Image image, int x, int y);
@@ -51,10 +56,12 @@ void setPixel(Image image, int x, int y, Pixel p);
 int getWidth(Image image);
 int getHeight(Image image);
 
+Color getPixelColor(Image image, int x, int y);
+void setPixelColor(Image image, int x, int y, Color c);
 
-Pixel** createPixelTab(int width, int height);
+Pixel** createPixelTab(int width, int height, Color c);
 
-Image createImage(int width, int height);
+Image createImage(int width, int height, Color c);
 
 void destroyImage(Image image);
 
