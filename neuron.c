f2f3b81a42289eb40cb4526e0f99683fe218e15c
Claire <claireb.besancon@gmail.com>
2025-03-04 15:20:14 +0100

    enlever les intputs dans neuron attention il faudra modifier les fonctions de tests en fonction de cela

--- neuron.c
+++ neuron.c
@@ -10,22 +10,41 @@ double transfer(double x) {
     return tanh(x);
 }
 
+double set_nbTab(Neuron *neuron) {
+    int len = 0
+    while (neuron.weigts[len] != NULL){
+        len++;
+    }
+    return len;
+}
+
 // Fonction de propagation 
-double forward(Neuron *neuron, double inputs[INPUT_SIZE]) {
+double forward(Neuron *neuron, double* entree) {
     double sum = 0;
-    for (int i = 0; i < INPUT_SIZE; i++) {
-        sum += neuron->weights[i] * inputs[i];
+    for (int i = 0; i < set_nbSynapse(Neuron); i++) {
+        sum += neuron->weights[i] * entree[i];
     }
     return transfer(sum);
 }
 
 // Fonction pour modifier la valeur des poids d'un neurone
-void setWeights(Neuron *neuron, double new_weights[INPUT_SIZE]) {
-    for (int i = 0; i < INPUT_SIZE; i++) {
+void setWeights(Neuron *neuron, double* new_weights) {
+    for (int i = 0; i < set_nbSynapse(Neuron); i++) {
         neuron->weights[i] = new_weights[i];
     }
 }
 
+void init_neuron(Couche* curr_couche, int nb_synapses){
+    if (curr_couche.p != NULL)
+    {
+        for (int i = 0; i < nb_neurones; i++) {
+            for (int j = 0; j < nb_synapse; j++) {
+                couche->tab_n[i].weights[j] = ((double)rand() / RAND_MAX) * 2 - 1; 
+            }
+        }
+        init_neuron(curr_couche.p, curr_couche.nb_neurones);
+    }
+}
 
 // Initialisation d'une couche
 Couche *init_couche(int nb_neurones, Couche *couche_suivante, int is_fst_couche, int is_lst_couche) {
@@ -43,18 +62,29 @@ Couche *init_couche(int nb_neurones, Couche *couche_suivante, int is_fst_couche,
         exit(1);
     }
 
-    for (int i = 0; i < nb_neurones; i++) {
-        for (int j = 0; j < INPUT_SIZE; j++) {
-            couche->tab_n[i].weights[j] = ((double)rand() / RAND_MAX) * 2 - 1; 
-        }
-    }
-
     couche->p = couche_suivante;
     couche->is_fst_couche = is_fst_couche;
     couche->is_lst_couche = is_lst_couche;
 
     return couche;
 }
+
+
+Couche *init_reseau(int nb_couches, int taille_max, int taille_min, int nb_entrees, int nb_sorties) {
+	// creation couche de sortie (nb_sorties = nb_neurones)
+    Couche *lst_couche = init_couche(nb_sorties, NULL, 0, 1);
+    Couche *temp = lst_couche;
+	// ajout des couches cachées (leurs nb de neurones est aléatoire)
+    for (int i = 1; i < nb_couches - 1; i++) {
+        int taille_courante = rand() % (taille_max - taille_min + 1) + taille_min;
+        //chaque nouvelle couche pointe vers la précédente
+        temp = init_couche(taille_courante, temp, 0, 0);
+    }
+    Couche *fst_couche = init_couche(nb_entrees, temp, 1, 0);
+    init_neuron(fst_couche, nb_entrees);
+    return fst_couche;
+}
+
 
 
 double *calcul_couche(Couche *couche, double *tab_val, int input_size) {
@@ -81,21 +111,6 @@ double *calcul_reseau(double *tab_val, Couche *fst_couche) {
         current = current->p;
     }
     return result;
-}
-
-
-Couche *init_reseau(int nb_couches, int taille_max, int taille_min, int nb_entrees, int nb_sorties) {
-	// creation couche de sortie (nb_sorties = nb_neurones)
-    Couche *lst_couche = init_couche(nb_sorties, NULL, 0, 1);
-    Couche *temp = lst_couche;
-	// ajout des couches cachées (leurs nb de neurones est aléatoire)
-    for (int i = 1; i < nb_couches - 1; i++) {
-        int taille_courante = rand() % (taille_max - taille_min + 1) + taille_min;
-        //chaque nouvelle couche pointe vers la précédente
-        temp = init_couche(taille_courante, temp, 0, 0);
-    }
-    Couche *fst_couche = init_couche(nb_entrees, temp, 1, 0); 
-    return fst_couche;
 }
 
 void interpretation(double *tab_val_sortie, char *tab_sortie[], int len) {
--- neuron.h
+++ neuron.h
@@ -5,24 +5,28 @@
 #define INPUT_SIZE 2 // Nombre d'entrées (x et y)
 
 typedef struct {
-    double weights[INPUT_SIZE]; // tableau contenant les poids synaptiques
+    double* weights; // tableau contenant les poids synaptiques
 } Neuron;
 
 
 typedef struct Couche {
     Neuron *tab_n;        // Tableau de neurones
     struct Couche *p;     // Pointeur vers la couche suivante
+    //struct Couche *p;     // Pointeur vers la couche précédente
     int is_fst_couche;    // Indique si c'est la première couche
     int is_lst_couche;    // Indique si c'est la dernière couche
     int nb_neurones;      // Nombre de neurones dans la couche
 } Couche;
 
 double transfer(double x); // Fonction de transfert
-double forward(Neuron *neuron, double inputs[INPUT_SIZE]); // calcul de la sortie
+double forward(Neuron *neuron, double* entree); // calcul de la sortie
+void setWeights(Neuron *neuron, double* new_weights);
+void init_neuron(Couche* curr_couche, int nb_synapses);
 Couche *init_couche(int nb_neurones, Couche *couche_suivante, int is_fst_couche, int is_lst_couche); // Initialisation d'une couche
+Couche *init_reseau(int nb_couches, int taille_max, int taille_min, int nb_entrees, int nb_sorties); // Initialisation d'un réseau
 
 double *calcul_couche(Couche *couche, double *tab_val, int input_size); // Calcul de la sortie d'une couche
 double *calcul_reseau(double *tab_val, Couche *fst_couche); // Calcul de la sortie d'un réseau
-Couche *init_reseau(int nb_couches, int taille_max, int taille_min, int nb_entrees, int nb_sorties); // Initialisation d'un réseau
+
 
 #endif // NEURON_H
