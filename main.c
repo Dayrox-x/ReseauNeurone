a3056ec6161c17cb697c2fb5cf9d39cded6b09cd
Dayrox-x <paul.saraz1333@gmail.com>
2025-03-12 16:01:02 +0100

    ajout des logs
    ajout fichier log.c et log.h et ajout des lignes de logs dans le main.c

--- main.c
+++ main.c
@@ -7,80 +7,92 @@
 #include "population.h"
 #include "sdl.h"
 #include "backpropagation.h"
+#include "log.h"
+
+#define PIXEL_SIZE 10
+#define WIDTH 640/PIXEL_SIZE
+#define HEIGHT 640/PIXEL_SIZE
 
 // pour compiler : gcc *.c -g -o main -lm -lSDL2
 // -lm pour la librairie mathematique, -lSDL2 pour la librairie SDL2
 // pour executer : ./main
 // SFML et Doxygen
 
-#define PIXEL_SIZE 10
-#define WIDTH 640/PIXEL_SIZE
-#define HEIGHT 640/PIXEL_SIZE
- 
-int main( int argc, char* args[] ) {/*
-	//Demarrer SDL 
-	int ret = SDL_Init( SDL_INIT_VIDEO );
-	assert(ret == 0 && "SDL_Init failed");
+int main(int argc, char* args[]) {
+    log_info("Démarrage du programme");
 
-	SDL_Window* window = SDL_CreateWindow( "SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * PIXEL_SIZE, HEIGHT * PIXEL_SIZE, SDL_WINDOW_SHOWN );
-	assert(window != NULL && "SDL_CreateWindow failed");
+    log_debug("Initialisation de SDL");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        log_fatal("Échec de SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    log_info("SDL initialisé avec succès");
 
-	SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_SOFTWARE );
-	assert(renderer != NULL && "SDL_CreateRenderer failed");
+    SDL_Window* window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * PIXEL_SIZE, HEIGHT * PIXEL_SIZE, SDL_WINDOW_SHOWN);
+    if (!window) {
+        log_fatal("Échec de SDL_CreateWindow : %s", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    log_info("Fenêtre SDL créée avec succès");
 
-	ret = SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-	assert(ret == 0 && "SDL_SetRenderDrawColor failed");
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    if (!renderer) {
+        log_fatal("Échec de SDL_CreateRenderer : %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    log_info("Renderer SDL créé avec succès");
 
-	ret = SDL_RenderClear( renderer );
-	assert(ret == 0 && "SDL_RenderClear failed");
+    // Définition des couleurs
+    log_debug("Initialisation des couleurs");
+    Color red = createColor(255, 0, 0, 255);
+    Color blue = createColor(0, 0, 255, 255);
+    log_info("Couleurs initialisées");
 
-	Color white = createColor(255, 255, 255, 255);
-	Color red = createColor(255, 0, 0, 255);
-	Color blue = createColor(0, 0, 255, 255);
+    // Création de l'image
+    log_debug("Création de l'image");
+    Image image = createImage(WIDTH, HEIGHT, blue);
+    log_info("Image créée avec taille %dx%d", WIDTH, HEIGHT);
 
-	SDL_Rect pixel = {0, 0, PIXEL_SIZE, PIXEL_SIZE};
+    // Initialisation du réseau de neurones
+    log_debug("Initialisation du réseau de neurones");
+    Couche* reseau = init_reseau(10, 5, 2, 2, 2);
+    if (!reseau) {
+        log_fatal("Échec de l'initialisation du réseau");
+        destroyImage(image);
+        destroyColor(red);
+        destroyColor(blue);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    log_info("Réseau de neurones initialisé");
 
-	Image image = createImage(WIDTH, HEIGHT, white);
+    // Affichage du réseau avant apprentissage
+    log_debug("Affichage du réseau avant apprentissage");
+    print_reseau(reseau);
 
-	createPopulation(image, red, 1);
-	createPopulation(image, blue, -1);
+    // Phase d'apprentissage
+    log_warn("Début de l'apprentissage");
+    learn(reseau, image, 0.001, 0.01);
+    log_info("Fin de l'apprentissage");
 
-	renderImage(image, renderer, window, pixel, PIXEL_SIZE);
+    // Affichage du réseau après apprentissage
+    log_debug("Affichage du réseau après apprentissage");
+    print_reseau(reseau);
 
-	destroyImage(image);
+    // Nettoyage des ressources
+    log_debug("Libération des ressources");
+    destroyImage(image);
+    destroyColor(red);
+    destroyColor(blue);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    log_info("Ressources libérées, fin du programme");
 
-	destroyColor(white);
-	destroyColor(red);
-	destroyColor(blue);
-
-
-	SDL_Delay(50000);
-
-	SDL_RenderPresent( renderer );
- 
-	//Quitter SDL 
-	SDL_DestroyRenderer( renderer );
-	SDL_DestroyWindow( window );
-	SDL_Quit(); 
- */
-
-	Color red = createColor(255, 0, 0, 255);
-	Color blue = createColor(0, 0, 255, 255);
- 	Image image = createImage(WIDTH, HEIGHT, blue);
-
-	Couche* reseau = init_reseau(10, 5, 2, 2, 2);
-
-	print_reseau(reseau);
-
-	
-	learn(reseau, image, 0.001, 0.01);
-	print_reseau(reseau);
-
-
-
-	destroyImage(image);
-	destroyColor(red);
-	destroyColor(blue);
-
-	return 0; 
+    return EXIT_SUCCESS;
 }
